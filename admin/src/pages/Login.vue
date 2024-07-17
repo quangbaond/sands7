b6a43c306e67bca73590c4bd80a36538d39d8d4d
quangbaond <quangbaorp@gmail.com>
2024-07-17 15:34:17 +0700

    develop

--- admin/src/pages/Login.vue
+++ admin/src/pages/Login.vue
@@ -122,7 +122,7 @@ onMounted(() => {
 }
 
 #wrap {
-    background-image: url(@/assets/images/bg_login.PNG);
+    background-image: url(@/assets/images/bg_login.png);
     background-size: cover;
     background-position: center;
 }
