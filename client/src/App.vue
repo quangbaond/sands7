9c2605da7082fabf3ebc419e452389d868d5678f
quangbaond <quangbaorp@gmail.com>
2024-08-06 00:25:49 +0700

    develop

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,5 +1,15 @@
 <script setup>
+import axios from '@/common/axios';
+import { useStore } from 'vuex';
 
+const store = useStore();
+axios.get('/cskh').then(res => {
+  store.commit('setCskh', res);
+});
+
+axios.get('/me/profile').then(res => {
+  store.commit('setProfile', res?.user);
+})
 </script>
 
 <template>
